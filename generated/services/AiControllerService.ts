ed6f6339e9524d226d556468da3a084302d6254e
wanghewei <codehev@qq.com>
2025-03-01 14:48:52 +0800

    style: 格式化代码

--- generated/services/AiControllerService.ts
+++ generated/services/AiControllerService.ts
@@ -2,35 +2,33 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
-import type { BaseResponse_string_ } from '../models/BaseResponse_string_';
+import type { BaseResponse_string_ } from "../models/BaseResponse_string_";
 
-import type { CancelablePromise } from '../core/CancelablePromise';
-import { OpenAPI } from '../core/OpenAPI';
-import { request as __request } from '../core/request';
+import type { CancelablePromise } from "../core/CancelablePromise";
+import { OpenAPI } from "../core/OpenAPI";
+import { request as __request } from "../core/request";
 
 export class AiControllerService {
-
-    /**
-     * analysisCodeByAI
-     * @param id id
-     * @returns BaseResponse_string_ OK
-     * @throws ApiError
-     */
-    public static analysisCodeByAiUsingGet(
-id?: number,
-): CancelablePromise<BaseResponse_string_> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/api/analysisCodeByAI',
-            query: {
-                'id': id,
-            },
-            errors: {
-                401: `Unauthorized`,
-                403: `Forbidden`,
-                404: `Not Found`,
-            },
-        });
-    }
-
+  /**
+   * analysisCodeByAI
+   * @param id id
+   * @returns BaseResponse_string_ OK
+   * @throws ApiError
+   */
+  public static analysisCodeByAiUsingGet(
+    id?: number
+  ): CancelablePromise<BaseResponse_string_> {
+    return __request(OpenAPI, {
+      method: "GET",
+      url: "/api/analysisCodeByAI",
+      query: {
+        id: id,
+      },
+      errors: {
+        401: `Unauthorized`,
+        403: `Forbidden`,
+        404: `Not Found`,
+      },
+    });
+  }
 }
