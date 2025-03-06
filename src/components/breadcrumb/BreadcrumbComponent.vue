eeb3651e4d9b761db18f4d52edc45eff1ef60a9b
wanghewei <codehev@qq.com>
2025-03-06 13:21:29 +0800

    feat: 新增面包屑导航组件

---
+++ src/components/breadcrumb/BreadcrumbComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <a-breadcrumb class="container-breadcrumb">
+    <a-breadcrumb-item>
+      <icon-apps />
+    </a-breadcrumb-item>
+    <a-breadcrumb-item v-for="item in items" :key="item">
+      {{ item }}
+    </a-breadcrumb-item>
+  </a-breadcrumb>
+</template>
+
+<script lang="ts" setup>
+import { PropType, defineProps } from "vue";
+import { IconApps } from "@arco-design/web-vue/es/icon";
+
+defineProps({
+  items: {
+    type: Array as PropType<string[]>,
+    default() {
+      return [];
+    },
+  },
+});
+</script>
+
+<style scoped lang="less">
+.container-breadcrumb {
+  margin: 16px 0;
+
+  :deep(.arco-breadcrumb-item) {
+    color: rgb(var(--gray-6));
+
+    &:last-child {
+      color: rgb(var(--gray-8));
+    }
+  }
+}
+</style>
