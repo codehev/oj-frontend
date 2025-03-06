10b6f59d57b5d46b4713e077397dd2c36fb2de4e
wanghewei <codehev@qq.com>
2025-03-06 22:04:22 +0800

    refactor: 调整路由守卫文件结构，优化路由权限控制导入

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import store from "./store";
 import ArcoVue from "@arco-design/web-vue";
 import "@icon-park/vue-next/styles/index.css";
 import "@arco-design/web-vue/dist/arco.css";
-import "@/access"; //默认引入@/access/index.ts
+import "@/access/routeGuard"; //全局路由守卫
 import "bytemd/dist/index.css"; //bytemd下载命令npm i @bytemd/vue-next, npm i @bytemd/plugin-gfm @bytemd/plugin-highlight
 
 createApp(App).use(store).use(router).use(ArcoVue).mount("#app");
