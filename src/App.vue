3f6246a7a65f7c5919f00136868666497a35f26a
wanghewei <codehev@qq.com>
2025-03-06 13:44:51 +0800

    refactor: 移除未使用的 UserLayout 导入

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,6 @@
 import BasicLayout from "@/layouts/BasicLayout.vue";
 import { onMounted } from "vue";
 import { useRoute } from "vue-router";
-import UserLayout from "@/layouts/UserLayout.vue";
 
 const route = useRoute();
 
