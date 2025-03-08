d3965c58c502b82136e55d30927f5f4160eb1e4b
wanghewei <codehev@qq.com>
2025-03-08 14:52:21 +0800

    feat: 新增 Minio 文件上传和下载服务方法

--- generated/services/FileControllerService.ts
+++ generated/services/FileControllerService.ts
@@ -11,6 +11,163 @@ import { request as __request } from '../core/request';
 export class FileControllerService {
 
     /**
+     * downloadFromMinio
+     * @param objectName objectName
+     * @returns any OK
+     * @throws ApiError
+     */
+    public static downloadFromMinioUsingGet(
+objectName?: string,
+): CancelablePromise<any> {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/api/file/downloadMinio',
+            query: {
+                'objectName': objectName,
+            },
+            errors: {
+                401: `Unauthorized`,
+                403: `Forbidden`,
+                404: `Not Found`,
+            },
+        });
+    }
+
+    /**
+     * downloadFromMinio
+     * @param objectName objectName
+     * @returns any OK
+     * @throws ApiError
+     */
+    public static downloadFromMinioUsingHead(
+objectName?: string,
+): CancelablePromise<any> {
+        return __request(OpenAPI, {
+            method: 'HEAD',
+            url: '/api/file/downloadMinio',
+            query: {
+                'objectName': objectName,
+            },
+            errors: {
+                401: `Unauthorized`,
+                403: `Forbidden`,
+            },
+        });
+    }
+
+    /**
+     * downloadFromMinio
+     * @param objectName objectName
+     * @returns any OK
+     * @throws ApiError
+     */
+    public static downloadFromMinioUsingPost(
+objectName?: string,
+): CancelablePromise<any> {
+        return __request(OpenAPI, {
+            method: 'POST',
+            url: '/api/file/downloadMinio',
+            query: {
+                'objectName': objectName,
+            },
+            errors: {
+                401: `Unauthorized`,
+                403: `Forbidden`,
+                404: `Not Found`,
+            },
+        });
+    }
+
+    /**
+     * downloadFromMinio
+     * @param objectName objectName
+     * @returns any OK
+     * @throws ApiError
+     */
+    public static downloadFromMinioUsingPut(
+objectName?: string,
+): CancelablePromise<any> {
+        return __request(OpenAPI, {
+            method: 'PUT',
+            url: '/api/file/downloadMinio',
+            query: {
+                'objectName': objectName,
+            },
+            errors: {
+                401: `Unauthorized`,
+                403: `Forbidden`,
+                404: `Not Found`,
+            },
+        });
+    }
+
+    /**
+     * downloadFromMinio
+     * @param objectName objectName
+     * @returns any OK
+     * @throws ApiError
+     */
+    public static downloadFromMinioUsingDelete(
+objectName?: string,
+): CancelablePromise<any> {
+        return __request(OpenAPI, {
+            method: 'DELETE',
+            url: '/api/file/downloadMinio',
+            query: {
+                'objectName': objectName,
+            },
+            errors: {
+                401: `Unauthorized`,
+                403: `Forbidden`,
+            },
+        });
+    }
+
+    /**
+     * downloadFromMinio
+     * @param objectName objectName
+     * @returns any OK
+     * @throws ApiError
+     */
+    public static downloadFromMinioUsingOptions(
+objectName?: string,
+): CancelablePromise<any> {
+        return __request(OpenAPI, {
+            method: 'OPTIONS',
+            url: '/api/file/downloadMinio',
+            query: {
+                'objectName': objectName,
+            },
+            errors: {
+                401: `Unauthorized`,
+                403: `Forbidden`,
+            },
+        });
+    }
+
+    /**
+     * downloadFromMinio
+     * @param objectName objectName
+     * @returns any OK
+     * @throws ApiError
+     */
+    public static downloadFromMinioUsingPatch(
+objectName?: string,
+): CancelablePromise<any> {
+        return __request(OpenAPI, {
+            method: 'PATCH',
+            url: '/api/file/downloadMinio',
+            query: {
+                'objectName': objectName,
+            },
+            errors: {
+                401: `Unauthorized`,
+                403: `Forbidden`,
+            },
+        });
+    }
+
+    /**
      * uploadFile
      * @param biz 
      * @param file 
@@ -27,6 +184,38 @@ file?: Blob,
             url: '/api/file/upload',
             query: {
                 'biz': biz,
+            },
+            formData: {
+                'file': file,
+            },
+            errors: {
+                401: `Unauthorized`,
+                403: `Forbidden`,
+                404: `Not Found`,
+            },
+        });
+    }
+
+    /**
+     * uploadFileToMinio
+     * @param biz biz
+     * @param file 
+     * @param fileName fileName
+     * @returns BaseResponse_string_ OK
+     * @returns any Created
+     * @throws ApiError
+     */
+    public static uploadFileToMinioUsingPost(
+biz?: string,
+file?: Blob,
+fileName?: string,
+): CancelablePromise<BaseResponse_string_ | any> {
+        return __request(OpenAPI, {
+            method: 'POST',
+            url: '/api/file/uploadMinio',
+            query: {
+                'biz': biz,
+                'fileName': fileName,
             },
             formData: {
                 'file': file,
