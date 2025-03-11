a6c98b68bdf3ccb30d34c829cda703f70cf68c7e
wanghewei <codehev@qq.com>
2025-03-11 18:18:13 +0800

    feat: 优化无权限页面和题目解答展示
    1. 在NoAuthView中新增403状态页面，提供返回按钮
    2. 在QuestionAnswers组件中使用MdPreview替换MdViewer
    3. 为题目解答添加空状态展示

--- src/views/exception/NoAuthView.vue
+++ src/views/exception/NoAuthView.vue
@@ -2,13 +2,22 @@
   <!--  默认以文件名为组件名，使用ESLint时组件名只能多单词命名-->
   <div id="noAuthView">
     <BreadcrumbComponent :items="items" />
-    <span>没有权限</span>
+    <template>
+      <a-result status="403" subtitle="无权限访问">
+        <template #extra>
+          <a-space>
+            <a-button type="primary" @click="router.back()">返回</a-button>
+          </a-space>
+        </template>
+      </a-result>
+    </template>
   </div>
 </template>
 <script setup lang="ts">
 import BreadcrumbComponent from "@/components/breadcrumb/BreadcrumbComponent.vue";
 import { ref } from "vue";
 import { BreadcrumbItem } from "@/components/breadcrumb/types";
+import router from "@/router";
 
 const items = ref<BreadcrumbItem[]>([
   {
--- src/views/question/doQuestion/components/QuestionAnswers.vue
+++ src/views/question/doQuestion/components/QuestionAnswers.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="tab-content">
     <div v-if="question?.answer">
-      <MdViewer :value="question.answer" />
+      <!-- <MdViewer :value="question.answer" /> -->
+      <MdPreview :modelValue="question.answer" />
     </div>
-    <div v-else>暂无题解</div>
+    <div v-else>
+      <a-result status="404" subtitle="暂无题解"> </a-result>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { defineProps } from "vue";
 import { Question } from "../../../../../generated";
-import MdViewer from "@/components/markdown/MdViewer.vue";
+import { MdPreview } from "md-editor-v3";
 
 defineProps({
   question: {
