09caf4f8307781e1aa79b7b3d2ee6656539aa3e8
wanghewei <codehev@qq.com>
2025-03-30 15:29:30 +0800

    refactor(question): 优化题目通过率的计算和显示
    - 在 QuestionsView.vue 文件中，修改了 acceptedRate 列的模板
    - 将接受率计算方式改为四舍五入的百分比
    - 保留原有的通过数和提交数显示

--- src/views/question/QuestionsView.vue
+++ src/views/question/QuestionsView.vue
@@ -75,7 +75,9 @@
       <template #acceptedRate="{ record }">
         {{
           `${
-            record.submitNum ? record.acceptedNum / record.submitNum : "0"
+            record.submitNum
+              ? (record.acceptedNum / record.submitNum) * 100
+              : "0"
           }% (${record.acceptedNum} / ${record.submitNum})`
         }}
       </template>
